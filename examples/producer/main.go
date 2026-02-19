ef1b34a8f701be235d70e9ec8379a99c40702845
Ondrej Sika <ondrej@ondrejsika.com>
2026-02-20 00:11:57 +0100

    feat(examples/producer): Add example producer

---
+++ examples/producer/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	simple_kafka "github.com/sikalabsx/simple-kafka-go/pkg/simple_kafka"
+)
+
+func main() {
+	sk := simple_kafka.SimpleKafkaNewWriter(
+		[]string{"localhost:9092"},
+		"example-topic",
+	)
+
+	i := 0
+	for {
+		key := []byte(fmt.Sprintf("%d", i))
+		value := []byte(fmt.Sprintf("Hello_%d", i))
+		err := sk.Produce(key, value)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Produced message with key=%s value=%s\n", key, value)
+		i++
+		time.Sleep(1 * time.Second)
+	}
+}
