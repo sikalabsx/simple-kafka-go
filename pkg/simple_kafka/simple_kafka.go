2fb5dd8e831b4ceecd799dfef4bd5885880687a7
Ondrej Sika <ondrej@ondrejsika.com>
2026-02-20 00:10:42 +0100

    feat(pkg): Add SimpleKafkaNewReader, SimpleKafkaNewWriter, Consume, Produce

---
+++ pkg/simple_kafka/simple_kafka.go
@@ -0,0 +1,44 @@
+package simple_kafka
+
+import (
+	"context"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type SimpleKafka struct {
+	reader *kafka.Reader
+	writer *kafka.Writer
+}
+
+func SimpleKafkaNewReader(brokers []string, topic, groupID string) SimpleKafka {
+	return SimpleKafka{
+		reader: kafka.NewReader(kafka.ReaderConfig{
+			Brokers: brokers,
+			Topic:   topic,
+			GroupID: groupID,
+		}),
+	}
+}
+
+func SimpleKafkaNewWriter(brokers []string, topic string) SimpleKafka {
+	return SimpleKafka{
+		writer: kafka.NewWriter(kafka.WriterConfig{
+			Brokers: brokers,
+			Topic:   topic,
+		}),
+	}
+}
+
+func (sk *SimpleKafka) Consume() (kafka.Message, error) {
+	msg, err := sk.reader.ReadMessage(context.Background())
+	return msg, err
+}
+
+func (sk *SimpleKafka) Produce(key, value []byte) error {
+	msg := kafka.Message{
+		Key:   key,
+		Value: value,
+	}
+	return sk.writer.WriteMessages(context.Background(), msg)
+}
